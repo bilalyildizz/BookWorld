3275eb946b7b0e55ead1cf367318f5ad181f5bed
Bilal Yıldız <bilal.yildizz00@gmail.com>
2021-12-18 10:42:04 +0300

    Deneme

--- BookWorld/Models/Book.cs
+++ BookWorld/Models/Book.cs
@@ -11,7 +11,7 @@ namespace BookWorld.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public int? PageNumber { get; set; }
-        public string Image  { get; set; }
+        
         public int AuthorId { get; set; }
         public int SubcategoryId { get; set; }
         public int? PublisherId { get; set; }
